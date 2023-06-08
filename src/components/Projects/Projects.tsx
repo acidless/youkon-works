50e2b41d65078110aa8ab1bed251f4db0befe5b4
Oleg <Oleg@DESKTOP-B5TOANR>
2023-06-08 14:35:34 +0300

    Added content

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,16 +1,20 @@
 import kp11Messenger from "../../images/kp11-messenger.png";
+import stylemate from "../../images/stylemate.png";
+import rubtidnizer from "../../images/rubtidnizer.png";
 import * as React from "react";
 import Project from "./Project";
 
 function Projects() {
-    return <section className="container mx-auto last-projects mb-28" id="portfolio">
-        <h2 className="mb-5 text-2xl">Последние проекты</h2>
-        <div className="justify-between w-full items-start grid grid-cols-3 gap-10">
-            <Project name="Мессенджер для колледжа" image={kp11Messenger} num={1}/>
-            <Project name="Мессенджер для колледжа" image={kp11Messenger} num={2}/>
-            <Project name="Мессенджер для колледжа" image={kp11Messenger} num={3}/>
-        </div>
+  return (
+    <section className="container mx-auto last-projects mb-28" id="portfolio">
+      <h2 className="mb-5 text-2xl">Последние проекты</h2>
+      <div className="justify-between w-full items-start grid grid-cols-3 gap-10">
+        <Project name="Мессенджер для колледжа" image={kp11Messenger} num={1} />
+        <Project name="Онлайн гардероб" image={stylemate} num={2} />
+        <Project name="Приложение органайзер" image={rubtidnizer} num={3} />
+      </div>
     </section>
+  );
 }
 
 export default Projects;
